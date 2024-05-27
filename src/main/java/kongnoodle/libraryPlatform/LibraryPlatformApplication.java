5bcf3c7ea7d568137f3ef7ffa7155bc1e1511651
yooonwodyd <dbswodyd00@naver.com>
2024-05-27 18:31:23 +0900

    초기 프로젝트 설정

---
+++ src/main/java/kongnoodle/libraryPlatform/LibraryPlatformApplication.java
@@ -0,0 +1,13 @@
+package kongnoodle.libraryPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPlatformApplication.class, args);
+	}
+
+}
