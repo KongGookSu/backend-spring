a916ce5cfe386cc05d2cf26deec5900152653246
yooonwodyd <dbswodyd00@naver.com>
2024-06-01 04:34:02 +0900

    #5 accoountUpdateRequest에 of 메서드 추가

--- src/main/java/kongnoodle/libraryPlatform/demo/feat/user/dto/AccountUpdateRequest.java
+++ src/main/java/kongnoodle/libraryPlatform/demo/feat/user/dto/AccountUpdateRequest.java
@@ -1,9 +1,14 @@
 package kongnoodle.libraryPlatform.demo.feat.user.dto;
 
+import kongnoodle.libraryPlatform.demo.feat.user.entity.Account;
+
 public record AccountUpdateRequest(
 	long id,
 	String email,
 	String vendor,
 	String nickname
 ) {
+	public static AccountUpdateRequest of (String email, String vendor, String nickname) {
+		return new AccountUpdateRequest(0, email, vendor, nickname);
+	}
 }
